7f31ba6dfac3b29b1494a7995fe035c6278b8a27
Blick <dima.8m8@gmail.com>
2022-08-21 12:08:28 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+import AddItem from './components/AddItem/AddItem';
+
+function App() {
+  return (
+    <Fragment>
+      <AddItem />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/AddItem/AddItem.js
@@ -0,0 +1,9 @@
+import { Fragment } from 'react';
+
+import classes from './AddItem.module.css';
+
+const AddItem = () => {
+  return <Fragment></Fragment>;
+};
+
+export default AddItem;
